d82f6ee239b4cfad1c57145bf0f0309515ab3aab
Gabor Molnar <gabormolnar92@gmail.com>
2014-09-02 01:09:16 +0200

    Document upload implemented

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,13 @@
 /* Route::get('/',function(){   
   return 'Cover page';
   }); */
-  
+
+if (Config::get('app.debug')) {
+    Route::get('/phpinfo', function() {
+        echo phpinfo();
+    });
+}
+
 Route::get('/', ['uses' => 'Site\HomeController@index', 'as' => 'fooldal']);
 
 Route::get('hirek/{id}/{title}', ['uses' => 'Site\ArticleController@show', 'as' => 'hirek.show'])->where('id', '[0-9]+')->where('title', '[0-9A-z_-]+');
@@ -30,6 +36,8 @@ Route::get('galeriak/{id}/{title}', ['uses' => 'Site\GalleryController@show', 'a
 
 Route::get('oldal/{id}/{title}', ['uses' => 'Site\PageController@show', 'as' => 'oldalak.show'])->where('id', '[0-9]+')->where('title', '[0-9A-z_-]+');
 
+Route::get('documentumok', ['uses' => 'Site\DocumentController@index', 'as' => 'dokumentumok.index']);
+
 /**
  * -----------------------------------------------------------------------------
  * Site menu
@@ -38,29 +46,31 @@ Route::get('oldal/{id}/{title}', ['uses' => 'Site\PageController@show', 'as' => 
  * A cms-hez tarozó menu-k. 
  * 
  */
+if (!Request::is('admin') && !Request::is('admin/*')) {
 
-Menu::make('mainMenu', function($menu) {
+    Menu::make('mainMenu', function($menu) {
 
-    $menu->add('Főoldal', array('route' => 'fooldal'));
+        $menu->add('Főoldal', array('route' => 'fooldal'));
 
-    $menu->add('Események', array('route' => 'esemenyek.index'));
+        $menu->add('Események', array('route' => 'esemenyek.index'));
 
-    $menu->add('Galériák', array('route' => 'galeriak.index'));
-
-    try{
-    \Divide\CMS\Page::getPagesForMenu($menu, 0);
-
-    foreach ($menu->all() as $item) {
-        if ($item->hasChildren()) {
-            $item->append('<i class="fa fa-bars"></i>');
-        }
-    }
-    }catch(\Exception $e){
+        $menu->add('Galériák', array('route' => 'galeriak.index'));
         
-    }
-});
+        $menu->add('Dokumentumok', array('route' => 'dokumentumok.index'));
 
+        try {
+            \Divide\CMS\Page::getPagesForMenu($menu, 0);
 
+            foreach ($menu->all() as $item) {
+                if ($item->hasChildren()) {
+                    $item->append('<i class="fa fa-bars"></i>');
+                }
+            }
+        } catch (\Exception $e) {
+            
+        }
+    });
+}
 
 
 /**
@@ -92,6 +102,8 @@ Route::group(array('prefix' => 'admin', 'namespace' => 'Admin', 'before' => 'use
     Route::resource('hir', 'ArticleController');
 
     Route::resource('esemeny', 'EventController');
+    
+    Route::resource('dokumentum', 'DocumentController');
 
     Route::resource('galeria', 'GalleryController');
 
@@ -105,14 +117,14 @@ Route::group(array('prefix' => 'admin', 'namespace' => 'Admin', 'before' => 'use
      * Felhasználók kezeléséhez tartozó route-ok.
      */
     Route::group(['prefix' => 'felhasznalok'], function() {
-        Route::resource('felhasznalo', 'UsersController');      
-        
+        Route::resource('felhasznalo', 'UsersController');
+
         Route::post('felhasznalo/{id}/change', ['uses' => 'UsersController@postProfile', 'as' => 'admin.felhasznalok.felhasznalo.change']);
-        
+
         Route::post('felhasznalo/{id}/password', ['uses' => 'UsersController@postPassword', 'as' => 'admin.felhasznalok.felhasznalo.password']);
-        
+
         Route::post('felhasznalo/{id}/picture', ['uses' => 'UsersController@postProfilePicture', 'as' => 'admin.felhasznalok.felhasznalo.picture']);
-        
+
         Route::get('felhasznalo/{id}/picture/delete', ['uses' => 'UsersController@deleteProfilePicture', 'as' => 'admin.felhasznalok.felhasznalo.delete.picture']);
 
         Route::resource('felhasznalo-csoport', 'GroupsController');
